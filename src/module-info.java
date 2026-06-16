a9fdc69218812b01b5ae665ef6afa17c0153c8d1
Melih <melih.acar@gmx.de>
2026-06-16 13:42:47 +0200

    Aufgabe 6b: Repository erstellt und module-info angepasst

--- src/module-info.java
+++ src/module-info.java
@@ -2,10 +2,10 @@ module Aufgabe6b {
     requires javafx.controls;
     requires common;
     requires org.junit.jupiter.api;
-    
+
     exports controller.upn;
     exports controller.upn.operator;
     exports model;
-    
+
     opens application to javafx.graphics, javafx.fxml;
 }
