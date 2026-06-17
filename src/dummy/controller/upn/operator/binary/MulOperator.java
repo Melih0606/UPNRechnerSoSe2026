d8e48a47cd5436f10cee077d776ac4dcc67c1c6e
kepio <piotrowski.kevin@fh-swf.de>
2026-06-17 13:53:03 +0200

    6b: Dummy-Operatoren für Addition, Subtraktion, Multiplikation und Potenz ergänzt

---
+++ src/dummy/controller/upn/operator/binary/MulOperator.java
@@ -0,0 +1,39 @@
+package dummy.controller.upn.operator.binary;
+
+import common.exception.GeneralUserException;
+import controller.upn.operator.Operator;
+import model.Stack;
+
+/**
+ * Dummy-Implementierung des Multiplikationsoperators.
+ *
+ * @author Melih Acar, Kevin Piotrowski und Dmitrij Ogulev
+ */
+public class MulOperator implements Operator
+{
+   @Override
+   public void calculate(Stack<Double> stack) throws GeneralUserException
+   {
+      if (stack == null)
+      {
+         throw new IllegalArgumentException(
+               "Der Stack darf nicht null sein.");
+      }
+
+      if (stack.size() < 2)
+      {
+         throw new GeneralUserException("Zu wenige Operanden.");
+      }
+
+      double x = stack.pop();
+      double y = stack.pop();
+      double result = y * x;
+
+      if (Double.isNaN(result) || Double.isInfinite(result))
+      {
+         throw new GeneralUserException("Ungueltiges Ergebnis.");
+      }
+
+      stack.push(result);
+   }
+}
